3a144589c523bfdff89b4f0d1b952d02074ab9c5
Karo <e12023150@student.tuwien.ac.at>
2024-11-24 21:45:40 +0100

    fixed naming bug

--- app/src/main/java/com/example/pocketbudget/ui/history/HistoryViewModel.java
+++ app/src/main/java/com/example/pocketbudget/ui/history/HistoryViewModel.java
@@ -1,14 +1,14 @@
-package com.example.pocketbudget.ui.dashboard;
+package com.example.pocketbudget.ui.history;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class HistoryViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public HistoryViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is dashboard fragment");
     }
--- app/src/main/java/com/example/pocketbudget/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/pocketbudget/ui/settings/SettingsFragment.java
@@ -1,4 +1,4 @@
-package com.example.pocketbudget.ui.notifications;
+package com.example.pocketbudget.ui.settings;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.pocketbudget.databinding.FragmentNotificationsBinding;
+import com.example.pocketbudget.databinding.FragmentSettingsBinding;
 
-public class NotificationsFragment extends Fragment {
+public class SettingsFragment extends Fragment {
 
-    private FragmentNotificationsBinding binding;
+    private FragmentSettingsBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        NotificationsViewModel notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
+        SettingsViewModel settingsViewModel =
+                new ViewModelProvider(this).get(SettingsViewModel.class);
 
-        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        binding = FragmentSettingsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        settingsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
